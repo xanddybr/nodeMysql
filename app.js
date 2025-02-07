0cc5a366693955ef3521f26ce9bb808c1c0c9e68
xanddybr <xanddybr@gmail.com>
2025-02-07 16:36:31 -0300

    set password in mysql

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 // import express module
-const express = require("express")
+const express = require("express");
+const { parse } = require("path");
+const { stringify } = require("querystring");
+const { json } = require("stream/consumers");
 
 // create app extend functions class express
 const app = express();
+app.use(express.json())
+
+const users = []
+
+app.post("/",(req,res)=>{
+    users.push(req.body)
+    res.status(201).json(users)
+})
 
 //route hello word
 app.get("/",function(req,res){
-    res.write("Congratulations, your make your first API with node!")
-    res.end()
+    res.status(200).json(users)
 })
 
 //create server
