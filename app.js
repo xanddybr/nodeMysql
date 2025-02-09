48810c26235537e9ecd0f8594c6b4d6cda2452c3
xanddybr <xanddybr@gmail.com>
2025-02-09 13:40:29 -0300

    ok

--- app.js
+++ app.js
@@ -1,25 +1,55 @@
 // import express module
 const express = require("express");
-const { parse } = require("path");
-const { stringify } = require("querystring");
-const { json } = require("stream/consumers");
 
 // create app extend functions class express
 const app = express();
-app.use(express.json())
+const mysql = require("mysql2");
 
-const users = []
+const data = []
+
+const conexao = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Alex@1797",
+    database: "TEST"
+})
+
+conexao.connect((err)=>{
+    try{
+        if(err){
+            throw err = new Error("database unable to connect")
+        }
+
+    }catch(err){
+        console.log(err)
+    }
+})
+
+let sql = "SELECT * FROM contacts";
+
+conexao.query(sql,(err,result)=>{
+    if(err){
+        console.log(err)
+    }else{
+        data.push(result)
+    }
+})
 
 app.post("/",(req,res)=>{
     users.push(req.body)
-    res.status(201).json(users)
+    res.json("post method running...")
 })
 
 //route hello word
-app.get("/",function(req,res){
-    res.status(200).json(users)
+app.get("/",(req,res)=> {
+   res.json(data)
+   conexao.end()   
 })
 
-//create server
+app.get("/id/:id",(req,res)=> {
+    res.json(data[0][req.params.id])
+ })
 
+//create server
 app.listen(3000)
