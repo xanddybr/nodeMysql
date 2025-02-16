0f9ffbb999055373298f5445adebfee1f4ab1dfc
xanddybr <xanddybr@gmail.com>
2025-02-15 23:56:07 -0300

    nothing spacial

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ const express = require("express")
 const crude = require("./ContactClass") 
 const app = express()
 
-
-
 //crud.selectNewId();
-setInterval(crude.selectNewId, 5000);
+const setruning = setInterval(crude.selectNewId, 5000);
 
 app.listen(3000,() => {
     console.log("Server is running on port 3000, Database is ready to use!")
